96391a12aab267b1800ff9cbf275c9f85fb682ac
kali <cliffordjones954@icloud.com>
2026-04-25 06:52:28 -0400

    first commit

---
+++ week31.py
@@ -0,0 +1,7 @@
+First_name = input ("What is your first name? ")
+Last_name = input ("What is your last name? ")
+Age = input ("What is your age? ")
+
+print ("My full name is " + First_name + " " + Last_name)
+print ("My age is " + Age)  
+
